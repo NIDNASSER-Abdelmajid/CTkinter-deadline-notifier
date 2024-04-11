c7845ab51c6d7e28497082a0d8539cb7a92bd592
NIDNASSER abdelmajid <abdelmajidnidnasser1@gmail.com>
2024-04-11 03:15:51 +0000

    second push

--- Readme.md
+++ Readme.md
@@ -2,8 +2,11 @@ To Do:
 - Hide credentials [CRITIC]
 - button function: write in database, delete after deadline is met
 - function to validate entry type
+- fix error message label
+- show deadlines in a list
 
 Done:
 - create deadline database, deadlines table
 - grant select, insert, delete permissions on deadlines table to joyboy.
 - connect to mysql database 
+- add icon
--- deadline.py
+++ deadline.py
@@ -2,7 +2,9 @@ import customtkinter
 from mysql.connector import connect
 
 # database named: deadline
-mydb = connect(host="localhost", user="joyboy", password="joyboy")
+# table named: deadlines
+mydb = connect(host="localhost", database="deadline", user="joyboy", password="joyboy")
+cursor = mydb.cursor()
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -12,25 +14,35 @@ root = customtkinter.CTk()
 root.geometry("500x500")
 root.resizable(False, False)
 root.title("deadline")
+# icon
+root.wm_iconbitmap("icon.ico")
 
 
-def notValidEntry():
-    errMsg = customtkinter.CTkLabel(
-        master=frame,
-        text='invalid entry, should be "YYYY-MM-DD"',
-        font=("Roboto", 12),
-        text_color="Red",
-    )
-    errMsg.pack(before=deadline)
+def validEntry(text: str, date: str) -> bool:
+    return True
 
 
-def addToDatabase():
-    # if
-    #     print("00000")
-    #     label.destroy()
+def mysqlAction(action: int, text: str, date: str, tag: str):
+    "1- SELECT, 2- INSERT, 3-DELETE"
+    if action == 1:
+        return "SELECT * FROM deadlines;"
+    elif action == 2:
+        return f"INSERT INTO deadlines(EVENT, deadline) VALUES('{text}', '{date}', '{tag}');"
+    else:
+        return ""
 
-    notValidEntry()
-    print(f"{entry.get()}: {deadline.get()}")
+
+def addToDatabase() -> None:
+    text = entry.get()
+    date = deadline.get()
+    if validEntry(text, date):
+        cursor = mydb.cursor()
+        cursor.execute(mysqlAction(2, text, date, tag))
+        mydb.commit()
+        print(cursor.rowcount, "Record inserted successfully into Laptop table")
+        cursor.close()
+
+    # print(f"{}: {}")
 
 
 # frames
@@ -47,7 +59,9 @@ label.pack(pady=7, padx=10)
 entry = customtkinter.CTkEntry(frame, placeholder_text="add event", width=250)
 entry.pack(pady=7, padx=10)
 deadline = customtkinter.CTkEntry(frame, placeholder_text="YYYY-MM-DD", width=100)
-deadline.pack(pady=5, padx=10)
+deadline.pack(anchor="n", side="left", fill="x", pady=5, padx=5)
+tag = customtkinter.CTkEntry(frame, placeholder_text="tag: #taghere", width=100)
+tag.pack(anchor="n", side="left", fill="x", pady=5, padx=5)
 
 # Buttons
 submitBtn = customtkinter.CTkButton(
@@ -56,6 +70,20 @@ submitBtn = customtkinter.CTkButton(
     command=addToDatabase,
     text="Submit",
 )
-submitBtn.pack(pady=10, padx=60)
+submitBtn.pack(anchor="n", side="left", fill="x", pady=5, padx=10)
+
+# treeview
+table = customtkinter.CTkTabview(frame)
+table.pack(fill="both", pady=20, padx=10)
+
+
+# label
+errMsg = customtkinter.CTkLabel(
+    master=frame,
+    text='invalid entry, should be "YYYY-MM-DD"',
+    font=("Roboto", 12),
+    text_color="Red",
+)
+errMsg.pack(fill="both", pady=20, padx=10)
 
 root.mainloop()
